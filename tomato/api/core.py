49e2054f376a8d106c57390b692e9a594b6b17d4
soasme <soasme@gmail.com>
2016-07-02 12:49:03 +0800

    define micro for tomato.api.

---
+++ tomato/api/core.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from flask import abort
+from flask_jsonrpc.proxy import ServiceProxy
+
+class MicroservicesProxy(ServiceProxy):
+
+    def __call__(self, *args, **kwargs):
+        resp = super(MicroservicesProxy, self).__call__(*args, **kwargs)
+        if 'result' not in resp:
+            print(resp)
+            abort(500)
+        return resp
+
+class Micro(object):
+
+    def __getattr__(self, service):
+        gateway_url = current_app.config['GATEWAY_URL']
+        service_url = '%s/%s/jsonrpc' % (gateway_url, service)
+        service_proxy = MicroservicesProxy(service_url)
+        return service_proxy
+
+micro = Micro()
