8b5eee10b4946080b811f1e1165d3329b940fdb7
soasme <soasme@gmail.com>
2016-07-02 12:49:15 +0800

    Init tomato.api.app.

---
+++ tomato/api/app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+import click
+from flask import Flask
+
+from tomato.api.v1 import bp as v1bp
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object('tomato.api.settings')
+    app.config.from_envvar('API_SETTINGS', silent=True)
+    print('Running application in %s mode' % (app.debug and 'DEBUG' or 'NON-DEBUG'))
+
+    app.register_blueprint(v1bp, url_prefix='/1')
+
+    return app
+
